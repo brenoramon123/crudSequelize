c2011df5888f02678d671a4d879c9b0bde8fa9f1
Breno Ramon <brenoramon55@gmail.com>
2024-04-17 00:55:09 -0300

    .

--- routes/livros.routes.js
+++ routes/livros.routes.js
@@ -9,11 +9,33 @@ router.post('/create', async (req, res) => {
     try {
         console.log(req.body)
         const novoLivro = await Livro.create(req.body);
-        res.status(201).send(novoLivro);
+        res.render("home");
     } catch (error) {
         console.error(error);
         res.status(500).send('Erro ao adicionar livro');
     }
 });
 
+router.get("/",async (req, res) => {
+    try {
+        const livro = await Livro.findAll({raw:true});
+        console.log(livro)
+        res.status(201).render("home",{livro});
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Erro ao mostrar livros');
+    }
+})
+
+router.get("/verMais/:id",async (req, res) => {
+    try {
+        const id = req.params.id
+        const livro = await Livro.findOne({raw:true, where:{id:id}});
+        console.log(livro)
+        res.render("detalhe",{livro});
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Erro ao mostrar livros');
+    }
+})
 module.exports = router;
